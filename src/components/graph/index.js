a4e68e9c6c03792bd6c94d9b9d1f3847adf2983e
Abdinasir Yussuf <Abdinasir.yussuf@outlook.com>
2022-08-18 16:33:07 -0700

    netfliy run build error

--- src/components/graph/index.js
+++ src/components/graph/index.js
@@ -4,6 +4,7 @@ import Container from '@mui/material/Container';
 import {useEffect} from 'react';
 import {useSelector} from 'react-redux';
 import {useDispatch} from 'react-redux';
+
 import { getSP500 } from "../../store/yahooApi";
 import { getTrades} from "../../store/quiverApi";
 
@@ -18,50 +19,50 @@ import {
   ComposedChart
 } from "recharts";
 
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    // pv: 2400,
-    amt: 2400
-  },
-  {
-    name: "Page B",
-    uv: -3000,
-    // pv: 1398,
-    amt: 2210
-  },
-  {
-    name: "Page C",
-    uv: -2000,
-    // pv: -9800,
-    amt: 2290
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    // pv: 3908,
-    amt: 2000
-  },
-  {
-    name: "Page E",
-    uv: -1890,
-    // pv: 4800,
-    amt: 2181
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    // pv: -3800,
-    amt: 2500
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    // pv: 4300,
-    amt: 2100
-  }
-];
+// const data = [
+//   {
+//     name: "Page A",
+//     uv: 4000,
+//     // pv: 2400,
+//     amt: 2400
+//   },
+//   {
+//     name: "Page B",
+//     uv: -3000,
+//     // pv: 1398,
+//     amt: 2210
+//   },
+//   {
+//     name: "Page C",
+//     uv: -2000,
+//     // pv: -9800,
+//     amt: 2290
+//   },
+//   {
+//     name: "Page D",
+//     uv: 2780,
+//     // pv: 3908,
+//     amt: 2000
+//   },
+//   {
+//     name: "Page E",
+//     uv: -1890,
+//     // pv: 4800,
+//     amt: 2181
+//   },
+//   {
+//     name: "Page F",
+//     uv: 2390,
+//     // pv: -3800,
+//     amt: 2500
+//   },
+//   {
+//     name: "Page G",
+//     uv: 3490,
+//     // pv: 4300,
+//     amt: 2100
+//   }
+// ];
 
 const dataTwo = [
   {
@@ -103,27 +104,36 @@ const dataTwo = [
 
 export default function App() {
 
+  // const [getTrades, setTrades ] = useState(getTrades());
 
 
+
+  
   let dispatch = useDispatch();
 
  let yahooData = useSelector(state => state.yahoo);
 
  useEffect(()=>{
-  dispatch(getSP500());
+  let sP500 = getSP500();
+  console.log('getSP500 useEffect');
+  dispatch(sP500);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
 },[])
 
-let quiverData = useSelector(state => state.quiver.tradesOnDate);
+// let quiverData = useSelector(state => state.quiver.tradesOnDate);
 
 useEffect(()=>{
-  dispatch(getTrades());
+  let getRepTrades = getTrades();
+  console.log('get Trades useEffect');
+  dispatch(getRepTrades);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
 },[])
 
 
 
-  console.log('PROOF IN TABLE yahooData',yahooData);
+  // console.log('PROOF IN TABLE yahooData',yahooData);
 
-  console.log('PROOF IN TABLE quiverData',quiverData);
+  // console.log('PROOF IN TABLE quiverData',quiverData);
 
   return (
     <>
